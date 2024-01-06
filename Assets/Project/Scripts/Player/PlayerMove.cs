f76e8b427ad6257d6cf0ef14db4fdf1ef3b5624c
yshi112358 <76849725+yshi112358@users.noreply.github.com>
2024-01-06 15:53:51 +1000

    Change the Name of the File

---
+++ Assets/Project/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Game.Player
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        private float _pitch = 0, _yaw = 0, _roll = 0;
+        private float _pitchRate, _yawRate, _rollRate;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            Input.gyro.enabled = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var _tilt = transform.position.x;
+            _tilt += Input.gyro.rotationRateUnbiased.y / 100;
+            if (_tilt < -1f)
+                _tilt = -1f;
+            else if (_tilt > 1f)
+                _tilt = 1f;
+            transform.position = new Vector3(_tilt, transform.position.y, 0);
+        }
+    }
+}
