8a62c366b6665be3150929ae21eb8b2e6f6d36b2
yshi112358 <76849725+yshi112358@users.noreply.github.com>
2024-01-06 22:34:57 +1000

    Adjust NekoDrawer for UniRx

--- Assets/Project/Scripts/Neko/NekoDrawer.cs
+++ Assets/Project/Scripts/Neko/NekoDrawer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
 public class NekoDrawer : MonoBehaviour
 {
@@ -11,21 +13,25 @@ public class NekoDrawer : MonoBehaviour
     private List<int> _indexList = new List<int>();
     private Transform[] _child;
 
-    void Awake ()
+    void Awake()
     {
         _meshFilter = GetComponent<MeshFilter>();
         _child = new Transform[transform.childCount];
 
         _mesh = CreatePlaneMesh();
         _meshFilter.mesh = _mesh;
-    }
-    void Update ()
-    {
-        for (var i = 0; i < _vertextList.Count; i++)
-        {
-            _vertextList[i] = _child[i].position-transform.position;//全頂点のxとyをランダムでちょっと動かす
-        }
-        _mesh.SetVertices(_vertextList);
+
+        this.UpdateAsObservable()
+            .Where(_ => transform.parent.gameObject.activeSelf)
+            .Subscribe(_ =>
+            {
+                for (var i = 0; i < _vertextList.Count; i++)
+                {
+                    _vertextList[i] = _child[i].position - transform.position;
+                }
+                _mesh.SetVertices(_vertextList);
+            })
+            .AddTo(this);
     }
 
     private Mesh CreatePlaneMesh()
@@ -34,20 +40,19 @@ public class NekoDrawer : MonoBehaviour
         _vertextList.Clear();
         _uvList.Clear();
         _indexList.Clear();
-        
-        for(int i=0;i<transform.childCount;i++)
+
+        for (int i = 0; i < transform.childCount; i++)
         {
             _child[i] = transform.GetChild(i);
-            _vertextList.Add(_child[i].position-transform.position);
-            _uvList.Add(_child[i].localPosition+new Vector3(0.5f,0.5f,0));
-            // _uvList.Add(_child[i].localPosition+new Vector3(0.5f,0.5f,0));
+            _vertextList.Add(_child[i].position - transform.position);
+            _uvList.Add(_child[i].localPosition + new Vector3(0.5f, 0.5f, 0));
         }
 
-        _indexList.AddRange(new []{0,1,7,1,2,7,6,7,2,2,3,6,5,6,3,4,5,3});//0-2-1の頂点で1三角形。 1-2-3の頂点で1三角形。
+        _indexList.AddRange(new[] { 0, 1, 7, 1, 2, 7, 6, 7, 2, 2, 3, 6, 5, 6, 3, 4, 5, 3 });//0-2-1の頂点で1三角形。 1-2-3の頂点で1三角形。
 
         _mesh.SetVertices(_vertextList);//meshに頂点群をセット
-        _mesh.SetUVs(0,_uvList);//meshにテクスチャのuv座標をセット（今回は割愛)
-        _mesh.SetIndices(_indexList.ToArray(),MeshTopology.Triangles, 0);//メッシュにどの頂点の順番で面を作るかセット
+        _mesh.SetUVs(0, _uvList);//meshにテクスチャのuv座標をセット（今回は割愛)
+        _mesh.SetIndices(_indexList.ToArray(), MeshTopology.Triangles, 0);//メッシュにどの頂点の順番で面を作るかセット
         return _mesh;
     }
 }
