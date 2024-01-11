4ea8e77ba1afe058ef515d3a262c3573e0055432
yshi112358 <76849725+yshi112358@users.noreply.github.com>
2024-01-12 05:18:20 +1000

    Fix the Problem that app is suddenly stopped
    This app was stopped on my Galaxy Z Fold 5, but on other phone it completely worked.
    Thus the problem was caused by the memory on the phone.

--- Assets/Project/Scripts/Manager/SceneStateManager.cs
+++ Assets/Project/Scripts/Manager/SceneStateManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,11 +7,13 @@ namespace Game.Manager
 {
     public class SceneStateManager : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI _text;
         void Awake()
         {
             // シーンがない場合は、最初のシーンをセットする
             if (SceneState.sceneName.Value == null)
                 SceneState.SetSceneName(SceneManager.GetSceneAt(0).name);
+            DontDestroyOnLoad(gameObject);
         }
         public void LoadScene(string sceneName)
         {
@@ -23,23 +26,40 @@ namespace Game.Manager
         public void UnloadScene(string sceneName)
         {
             StartCoroutine(UnLoadSceneCo(sceneName));
-            Debug.Log("SceneStateManager.UnloadScene: " + sceneName + " is not found.");
         }
 
         private IEnumerator Load(string sceneName, bool isAdditive = false)
         {
             var activeScene = SceneManager.GetActiveScene().name;
             yield return StartCoroutine(LoadSceneCo("Loading"));
+            _text.text += "Loading" + "\n";
 
             yield return new WaitForSeconds(1f);
+            _text.text += "Wait" + "\n";
 
-            yield return StartCoroutine(LoadSceneCo(sceneName));
-            yield return new WaitUntil(() => SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName)));
-
-            SceneState.SetLoadEnd(true);
+            var loadingPath = SceneManager.GetSceneByName("Loading");
+            yield return new WaitUntil(() => SceneManager.SetActiveScene(loadingPath));
+            _text.text += "Active: Loading" + "\n";
 
             if (!isAdditive)
                 yield return StartCoroutine(UnLoadSceneCo(activeScene));
+            _text.text += "UnLoad: " + activeScene + "\n";
+            
+            Resources.UnloadUnusedAssets();
+            _text.text += "UnloadUnusedAssets" + "\n";
+
+            yield return StartCoroutine(LoadSceneCo(sceneName));
+            _text.text += "Load: " + sceneName + "\n";
+
+            var scenePath = SceneManager.GetSceneByName(sceneName);
+            _text.text += "Path: " + scenePath + "\n";
+
+            yield return new WaitUntil(() => SceneManager.SetActiveScene(scenePath));
+            _text.text += "Active: " + sceneName + "\n";
+
+            SceneState.SetLoadEnd(true);
+            _text.text += "LoadEnd" + "\n";
+
         }
 
         private IEnumerator LoadSceneCo(string sceneName)
@@ -47,20 +67,19 @@ namespace Game.Manager
             var isSceneExist = false;
             for (int i = 0; i < SceneManager.sceneCount; i++)
             {
+                _text.text += SceneManager.GetSceneAt(i).name + "\n";
                 if (SceneManager.GetSceneAt(i).name == sceneName)
                 {
                     isSceneExist = true;
                     break;
                 }
             }
+            _text.text += "isSceneExist: " + isSceneExist + "\n";
             if (isSceneExist)
                 yield break;
 
-            var async = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-            while (!async.isDone)
-            {
-                yield return null;
-            }
+            _text.text += "LoadSceneAsync" + "\n";
+            yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
         }
         private IEnumerator UnLoadSceneCo(string sceneName)
         {
